d66357bae6dd4405ba531af91a0d220a91a68925
moskovicher <71636683+moskovicher@users.noreply.github.com>
2024-12-12 15:31:34 +0200

    Update Fig3.m

--- Figures/Fig3.m
+++ Figures/Fig3.m
@@ -19,7 +19,7 @@ xlabel('T1w/T2w [a.u.]');
 subplot(2,3,3);
 [ax,R3]=plothist(nBins,T1woverPDw_all,R1_all,fontsize);
 xlabel('T1w/PDw [a.u.]'); 
-% colorbar;
+
 %% R2 correlates:
 subplot(2,3,4);
 [ax,R1]=plothist(nBins,R2w_all,R2_all,fontsize);
@@ -33,3 +33,16 @@ xlabel('T1w/T2w [a.u.]');
 subplot(2,3,6);
 [ax,R3]=plothist(nBins,lnT2woverPDw_all,R2_all,fontsize);
 xlabel('ln(T2w/PDw) [a.u.]'); 
+
+%% Cross-validation:
+
+% For R1 correlations, we calculate the RMSEs:
+[RMSE1] = crossValidation_RMSE(T1w_all,R1_all,0.10,1000);
+[RMSE2] = crossValidation_RMSE(T1woverT2w_all1,R1_all,0.10,1000);
+[RMSE3] = crossValidation_RMSE(T1woverPDw_all,R1_all,0.10,1000);
+
+% For R2 correlations, we calculate the RMSEs:
+[RMSE4] = crossValidation_RMSE(R2w_all,R2_all,0.10,1000);
+[RMSE5] = crossValidation_RMSE(T1woverT2w_all2,R2_all,0.10,1000);
+[RMSE6] = crossValidation_RMSE(lnT2woverPDw_all,R2_all,0.10,1000);
+
